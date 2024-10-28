fe9e3bece6d21dc503565478954a0f7ec43b4f52
Samarth Pradhan <103094468+samthewinner@users.noreply.github.com>
2024-10-28 21:42:28 +0530

    Update index.js

--- user-service/index.js
+++ user-service/index.js
@@ -1,17 +1,5 @@
-// import config from "./config";
-
 import { config } from "dotenv";
 import { resolve } from "path";
-
-// Setting __filename and __dirname for ES modules
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// Load environment variables from .env file
-// config({ path: resolve(__dirname, ".env") });
 
 import { registerWithEureka } from "./eureka-client.js";
 import express, { json } from "express";
@@ -30,18 +18,16 @@ const port = 3000;
 // Register the service with Eureka
 registerWithEureka('node-service', port);
 
-// Middleware to parse incoming JSON requests
 app.use(json());
 
 // Register the /user route
 app.use('/user', registerRoute);
 app.use('/admin',adminRouter);
-// Define a basic route for testing
+
 app.get('/', (req, res) => {
   res.send('Hi from node world!');
 });
 
-// Start the server on port 3000
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
